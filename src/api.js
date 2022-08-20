31b78514bf6854143764b0e90eb29ac0c389dd0a
Finn <contact@bechinka.eu>
2022-08-20 08:38:20 +0200

    mongodb test code

--- src/api.js
+++ src/api.js
@@ -1,15 +1,38 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const mongoose = require("mongoose");
+
+mongoose.connect(SECRET_DB_CONNECTION_STRING);
+const Schema = mongoose.Schema;
+
+const userLogSchema = new Schema(
+    {
+        date: String,
+        version: Number,
+    },
+    { collection: "user_log" }
+);
+
+const userLog = mongoose.model("UserLog", userLogSchema);
 
 const app = express();
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({
-        host: process.env.SECRET_DB_HOST,
-        user: process.env.SECRET_DB_USER,
-        pw: process.env.SECRET_DB_PW,
+    userLog.find().then((data) => {
+        res.json(data);
     });
+});
+
+router.get("/new", (req, res) => {
+    const tuple = {
+        date: Date.now().toString(),
+        version: 0.0,
+    };
+
+    const data = new userLog(tuple);
+    data.save();
+    res.status(200).send("OK");
 });
 
 app.use(`/.netlify/functions/api`, router);
