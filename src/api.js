73d625fdddc4bf2cb9c48d25776c968da63a9e60
Finn <contact@bechinka.eu>
2022-08-23 20:49:56 +0200

    added try catches to routes

--- src/api.js
+++ src/api.js
@@ -24,25 +24,33 @@ app.use(express.json());
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    userLog.find().then((data) => {
-        res.json(data);
-    });
+    try {
+        userLog.find().then((data) => {
+            res.json(data);
+        });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 router.post("/", (req, res) => {
-    const { app, version } = req.body;
-    if (app && version) {
-        const tuple = {
-            date: moment().local("de").format("YYYY-MM-DD HH:mm:ss"),
-            app: app,
-            version: version,
-        };
+    try {
+        const { app, version } = req.body;
+        if (app && version) {
+            const tuple = {
+                date: moment().local("de").format("YYYY-MM-DD HH:mm:ss"),
+                app: app,
+                version: version,
+            };
 
-        const data = new userLog(tuple);
-        data.save();
-        res.status(200).send("OK");
-    } else {
-        res.status(400).send("FAILURE");
+            const data = new userLog(tuple);
+            data.save();
+            res.status(200).send("OK");
+        } else {
+            res.status(400).send("FAILURE");
+        }
+    } catch (error) {
+        console.log(error);
     }
 });
 
